cddae684ff832dc5ab3e0034bc92831d230bfa32
“Ashwin <ashwinvidiyala@gmail.com>
2017-12-21 09:50:02 -0600

    add random numbers to classes of new divs to distinguish them

--- contact_card.js
+++ contact_card.js
@@ -2,7 +2,12 @@ $(document).ready(function () {
   $('#form').submit(function() {
     var formSubmit = $(this).serializeArray();
     console.log(formSubmit);
-    $('.contacts').append("<div class='contact-card-front'>");
+    var random = Math.floor(Math.random() * 200);
+    var newClass = "contact-card-front " + random;
+    // var last_name = fromSubmit.foreach(function(value, index) {
+    //   if (index == 1) {return value.value;}});
+    //   console.log(last_name);
+    $('.contacts').append("<div class='" + newClass + "'>");
 
       formSubmit.forEach(function(value, index) {
         // console.log(value.value);
@@ -12,12 +17,15 @@ $(document).ready(function () {
         if (index == 0) {
           //The problem with this class approach is that this is
           //appended to EVERY occurrence of the class. Not good.
-          $('.contact-card-front').append(value.value);
+          $('.contact-card-front' + "." + random).append(value.value);
           // console.log(value.value);
         }
         if (index == 1) {
-          $('.contact-card-front').append(value.value);
+          $('.contact-card-front' + "." + random).append(" " + value.value);
           // console.log(value.value);
+        }
+        if (index == 2) {
+          $('.contact-card-front' + "." + random).append("<p>" + value.value + "</p>");
         }
       });
     $('.contacts').append("</div>");
